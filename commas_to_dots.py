4d3dfb4421296ee150a5f4b22b04db5606f5355b
PaulinaKomorek <paulina-komorek@wp.pl>
2019-10-04 21:59:18 +0200

    key argument bug fixed

--- commas_to_dots.py
+++ commas_to_dots.py
@@ -5,10 +5,10 @@ from os import path
 
 def main():
     filetypes = [("Text files", "*.txt"), ("All files", "*.*")]
-    filename = filedialog.askopenfilename(filetypes)
+    filename = filedialog.askopenfilename(filetypes=filetypes)
     if not path.exists(filename):
         return
-    newfilename = filedialog.asksaveasfilename(filetypes)
+    newfilename = filedialog.asksaveasfilename(filetypes=filetypes)
     file = open(filename, "r")
     if len(newfilename) <= 0:
         return
